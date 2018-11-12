2b94cccc703831cbe20a54719b57a0443be80862
Kamsy <olekakamsy@gmail.com>
2018-11-12 16:44:05 +0100

    Updates

---
+++ precache-manifest.7b06a3a7bd76adf87d2a69de6ef44872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aaf4a99dcd1aa304283",
+    "url": "/crud/static/js/runtime~main.1aaf4a99.js"
+  },
+  {
+    "revision": "9329508f32176ae5244d",
+    "url": "/crud/static/js/main.9329508f.chunk.js"
+  },
+  {
+    "revision": "2133d7a30f1a8feb0cf2",
+    "url": "/crud/static/js/1.2133d7a3.chunk.js"
+  },
+  {
+    "revision": "9329508f32176ae5244d",
+    "url": "/crud/static/css/main.ff5e3c8e.chunk.css"
+  },
+  {
+    "revision": "03d3de9063905b742531abd20a19dbea",
+    "url": "/crud/index.html"
+  }
+];
